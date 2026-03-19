844cf97ed88ddaae1f120d6be104d2b9ae5e0a5e
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-03-19 12:25:55 +0100

    bild

--- CPPKube/Build -No console.bat
+++ CPPKube/Build -No console.bat
@@ -1,10 +1,8 @@
 @echo off
 if not exist BUILD mkdir BUILD
-@echo off
-if not exist BUILD mkdir BUILD
 g++ -O3 -DNDEBUG "RayCPPKube\RayCPPKube.cpp" ^
     -I"./include" -I"./raylib/include" ^
     -L"./raylib/lib" ^
     -lraylib -lopengl32 -lgdi32 -lwinmm ^
-    -w -s ^
-    -o "BUILD\RayCPPKubeKonsole.exe"
+    -mwindows -s ^
+    -o "BUILD\RayCPPKube.exe"
